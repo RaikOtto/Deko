dfc630d61baab3d41f85ff145eea9bb9f729cf13
ottoraik <fubioinf@gmail.com>
2018-06-27 13:58:27 +0200

    update

--- Scripts/Cibersort_script.R
+++ Scripts/Cibersort_script.R
@@ -3,17 +3,18 @@ library("stringr")
 library("grid")     ## Need to attach (and not just load) grid package
 #library("pheatmap")
 
-meta_info = read.table("~/Deko/Misc/Deko_table.tsv",sep = "\t",header = T,stringsAsFactors = F)
+meta_info = read.table("~/Deko/Misc/Meta_information.tsv",sep = "\t",header = T,stringsAsFactors = F)
 colnames(meta_info) = str_replace(colnames(meta_info),pattern = "\\.","_")
-draw_colnames_45 <- function (coln, gaps, ...) {coord = pheatmap:::find_coordinates(length(coln), gaps);x = coord$coord - 0.5 * coord$size;  res = textGrob(coln, x = x, y = unit(1, "npc") - unit(3,"bigpts"), vjust = 0.5, hjust = 1, rot = 45, gp = gpar(...));  return(res)}
+draw_colnames_45 <- function (coln, gaps, ...) {coord = pheatmap:::find_coordinates(length(coln), gaps);x = coord$coord - 0.5 * coord$size;  res = textGrob(coln, x = x, y = unit(1, "npc") - unit(3,"bigpts"), vjust = 0.5, hjust = 1, rot = 90, gp = gpar(...));  return(res)}
 assignInNamespace(x="draw_colnames", value="draw_colnames_45",ns=asNamespace("pheatmap"))
 
 ###
 
-bam_data = read.table("~/Deko/Data/GSE73339.HGNC.tsv",sep ="\t", header = T)
-#bam_data = read.table("~/MAPTor_NET/BAMs/Kallisto_three_groups/Groetzinger_Scarpa.TPM.filtered.HGNC.Voom.TMM.normalized.tsv",sep ="\t", header = T)
+bam_data = read.table("~/Deko/Data/GSE73338.HGNC.tsv",sep ="\t", header = T)
+bam_data = read.table("~/Deko/Data/TPMs.Not_normalized.Controls_Groetzinger_Scarpa.88S.tsv",sep ="\t", header = T)
 colnames(bam_data) = str_replace(colnames(bam_data),pattern = "^X","")
 colnames(bam_data) = str_replace(colnames(bam_data),pattern = "\\.","_")
+bam_data[1:5,1:5]
 
 #condition = (meta_info$Included == "Yes") & (meta_info$NEC_NET %in% c("NEC","NET")) #& (meta_info$Grading %in% c("G3"))
 #include_list  = meta_info$Name[ condition ]
@@ -23,12 +24,21 @@ colnames(bam_data) = str_replace(colnames(bam_data),pattern = "\\.","_")
 #dim(bam_data)
 #bam_data[1:5,1:5]
 
-#count_data = read.table("~/Deko/Data/baron_data.tsv",sep ="\t", header = T, stringsAsFactors = F)
 count_data = read.table("~/Deko/Data/Count_data.Segerstolpe.tsv",sep ="\t", header = T, stringsAsFactors = F)
+count_data[1:5,1:5]
 
-#subtypes = read.table(  "~/Deko/Data/baron_subtypes.tsv",sep ="\t", header = T, stringsAsFactors = F)[,1]
+gene_length_t = read.table("~/Deko/Misc/gene_length.tsv",sep ="\t", header = T, stringsAsFactors = F)
+l_match = match(rownames(count_data), gene_length_t$hgnc_symbol, nomatch = 0)
+count_data = count_data[ l_match != 0,]
+l_match = match( gene_length_t$hgnc_symbol, rownames(count_data), nomatch = 0)
+gene_length = gene_length_t$transcript_length[l_match]
+x = count_data / gene_length
+count_data = t(x) * 1e6 / colSums(x)
+
 seg_meta = read.table("~/Deko/Data/Segerstolpe_Meta_info.tsv", sep ="\t", header = T)
-s_match = match(colnames(count_data), seg_meta$Extract.Name, nomatch = 0)
+
+count_data = t(count_data)
+s_match = match( colnames(count_data), seg_meta$Extract.Name, nomatch = 0)
 
 subtypes = as.character(seg_meta$Characteristics.cell.type.)[s_match]
 subtypes = str_replace_all(subtypes, pattern = " cell", "")
@@ -54,13 +64,14 @@ marker_genes = read.table(
   stringsAsFactors = F
 )
 
+#pancreasMarkers = list("Alpha" = marker_genes$alpha[marker_genes$alpha != ""],"Beta" = marker_genes$beta[marker_genes$beta != ""],"Gamma" = marker_genes$gamma[marker_genes$gamma != ""],"Delta" = marker_genes$delta[marker_genes$delta != ""],"Ductal" = marker_genes$ductal[marker_genes$ductal != ""],"Acinar" = marker_genes$acinar[marker_genes$acinar != ""])
 pancreasMarkers = list(
-  "Alpha" = marker_genes$alpha[marker_genes$alpha != ""],
-  "Beta" = marker_genes$beta[marker_genes$beta != ""],
-  "Gamma" = marker_genes$gamma[marker_genes$gamma != ""],
-  "Delta" = marker_genes$delta[marker_genes$delta != ""],
-  "Ductal" = marker_genes$ductal[marker_genes$ductal != ""],
-  "Acinar" = marker_genes$acinar[marker_genes$acinar != ""]
+  "Alpha" = marker_genes$alpha[1:3],
+  "Beta" = marker_genes$beta[1:3],
+  "Gamma" = marker_genes$gamma[1:3],
+  "Delta" = marker_genes$delta[1:3]
+  #"Ductal" = marker_genes$ductal[1],
+  #"Acinar" = marker_genes$acinar[1]
 )
 
 # count data baron
@@ -69,8 +80,8 @@ pancreasMarkers = list(
 
 #table(str_to_upper(subtypes))
 #table(str_to_upper(names(pancreasMarkers)))
-#count_data = count_data[,str_to_upper(subtypes) %in% str_to_upper(names(pancreasMarkers)) ]
-#subtypes = subtypes[str_to_upper(subtypes) %in% str_to_upper(names(pancreasMarkers)) ]
+count_data = count_data[ which(str_to_upper(subtypes) %in% str_to_upper(names(pancreasMarkers))), ]
+subtypes = subtypes[ str_to_upper(subtypes) %in% str_to_upper(names(pancreasMarkers)) ]
 dim(count_data)
 length(subtypes)
 
@@ -81,6 +92,7 @@ subtypes[subtypes == "alpha"] = "Alpha"
 subtypes[subtypes == "beta"] = "Beta"
 subtypes[subtypes == "gamma"] = "Gamma"
 subtypes[subtypes == "delta"] = "Delta"
+subtypes[subtypes == "epsilon"] = "Epsilon"
 subtypes[subtypes == "acinar"] = "Acinar"
 subtypes[subtypes == "ductal"] = "Ductal"
 
@@ -105,42 +117,34 @@ fit = bseqsc_proportions(eset, B, verbose = TRUE)
 res_coeff = t(fit$coefficients)
 res_cor   = fit$stats
 
+res_coeff[ is.na(res_coeff) ] = 0.0
+res_cor[ is.na(res_cor) ] = 0.0
+res_coeff[ res_cor[,"Correlation"] <= .3, ] = .0
+res_cor[ res_cor[,"Correlation"] <= .3, "Correlation" ] = 0.0
+
+
 ###
 
 meta_match = match( colnames(exprs(eset)), meta_info$Name, nomatch = 0 ) 
-meta_info$Deco_type[meta_match] = cell_type
-meta_info$Deco_cor[meta_match]  = as.character(res_cor[,2])
 
+meta_info$Deco_cor[meta_match]  = as.character(res_cor[,2])
 res_coeff_match = match( rownames(res_coeff), meta_info$Name, nomatch = 0 )
 res_cor_match = match( rownames(res_cor), meta_info$Name, nomatch = 0 )
 
 maxi = apply( res_coeff, FUN = which.max, MARGIN = 1 )
 cell_type = colnames(res_coeff)[maxi]
-#cell_type[res_cor[,"Correlation"] <= .3] = "Not_sig"
+rownames(meta_info) = meta_info$Name
+cell_type[res_cor[,"Correlation"] <= .3] = "Not_sig"
+meta_info$Deco_type[meta_match] = cell_type
 meta_data = meta_info[meta_match,]
 
-
-meta_info$Alpha = rep("",nrow(meta_info))
-meta_info$Beta = rep("",nrow(meta_info))
-meta_info$Gamma = rep("",nrow(meta_info))
-meta_info$Delta = rep("",nrow(meta_info))
-meta_info$Acinar = rep("",nrow(meta_info))
-meta_info$Ductal = rep("",nrow(meta_info))
-meta_info$Res_cor = rep("",nrow(meta_info))
-meta_info$Alpha[res_coeff_match] = as.data.frame(res_coeff)$Alpha
-meta_info$Beta[res_coeff_match] = as.data.frame(res_coeff)$Beta
-meta_info$Gamma[res_coeff_match] = as.data.frame(res_coeff)$Gamma
-meta_info$Delta[res_coeff_match] = as.data.frame(res_coeff)$Delta
-meta_info$Acinar[res_coeff_match] = as.data.frame(res_coeff)$Acinar
-meta_info$Ductal[res_coeff_match] = as.data.frame(res_coeff)$Ductal
-meta_info$Res_cor[res_cor_match] = as.data.frame(res_cor)$Correlation
+#meta_data = data.frame(
+#  "Sample" = colnames(exprs(eset)),
+#  "Deco_type" = cell_type
+#)
+#rownames(meta_data) = meta_data$Sample
 
 #write.table(meta_info, "~/Deko/Misc/Meta_information.tsv",sep="\t", row.names = F, quote = F)
-
-res_coeff[ is.na(res_coeff) ] = 0.0
-res_cor[ is.na(res_cor) ] = 0.0
-res_coeff[ res_cor[,"Correlation"] <= .3, ] = .0
-res_cor[ res_cor[,"Correlation"] <= .3, "Correlation" ] = 0.0
 
 pheatmap::pheatmap(
   t(res_coeff),
@@ -148,9 +152,9 @@ pheatmap::pheatmap(
   annotation_colors = aka3,
   annotation_legend = T,
   treeheight_col = 0,
-  show_colnames = F,
+  show_colnames = T,
   show_rownames = T,
   gaps_row = 20
 )
 
-write.table(meta_info, "~/Deko/Misc/Deko_table.tsv",sep ="\t", row.names = F, quote = F)
+#write.table(meta_info, "~/Deko/Misc/Deko_table.tsv",sep ="\t", row.names = F, quote = F)
