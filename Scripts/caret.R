202dfd25bed0797af48fb60fbfaa528be49ef295
ottoraik <raik.otto@hu-berlin.de>
2021-12-06 11:07:45 +0100

    update

--- Scripts/caret.R
+++ Scripts/caret.R
@@ -1,29 +1,35 @@
 library(mltest)
 
 performance_mat_deconvolution_grading_tertiary = read.table("~/Downloads/Deconvolution.Grading_tertiary.tsv",header = TRUE,  as.is = TRUE)
-
-#classifier_metrics <- ml_test(predicted_labels, true_labels, output.as.table = FALSE)
 classifier_metrics <- ml_test(performance_mat_deconvolution[,2], performance_mat_deconvolution[,1], output.as.table = TRUE)
 classifier_metrics
 
 ###
 
 performance_mat_expression_grading_tertiary = read.table("~/Downloads/Expression_grading_tertiary.tsv",header = TRUE,  as.is = TRUE)
-
-#classifier_metrics <- ml_test(predicted_labels, true_labels, output.as.table = FALSE)
 classifier_metrics <- ml_test(performance_mat_expression_grading_tertiary[,2], performance_mat_expression_grading_tertiary[,1], output.as.table = TRUE)
 classifier_metrics
 
-performance_mat_deconvolution_grading_binary = read.table("~/Downloads/Deconvolution.Grading_binary.tsv",header = TRUE,  as.is = TRUE)
+###
 
-#classifier_metrics <- ml_test(predicted_labels, true_labels, output.as.table = FALSE)
+performance_mat_deconvolution_grading_binary = read.table("~/Downloads/Deconvolution.Grading_binary.tsv",header = TRUE,  as.is = TRUE)
 classifier_metrics <- ml_test(performance_mat_deconvolution_grading_binary[,2], performance_mat_deconvolution_grading_binary[,1], output.as.table = TRUE)
 classifier_metrics
 
 ###
 
 performance_mat_expression_grading_binary = read.table("~/Downloads/Expression_grading_binary.tsv",header = TRUE,  as.is = TRUE)
-
-#classifier_metrics <- ml_test(predicted_labels, true_labels, output.as.table = FALSE)
 classifier_metrics <- ml_test(performance_mat_expression_grading_binary[,2], performance_mat_expression_grading_binary[,1], output.as.table = TRUE)
+classifier_metrics
+
+###
+
+performance_mat_deconvolution_NET_NEC = read.table("~/Downloads/Deconvolution.NET_NEC.tsv",header = TRUE,  as.is = TRUE)
+classifier_metrics <- ml_test(performance_mat_deconvolution_NET_NEC[,2], performance_mat_deconvolution_NET_NEC[,1], output.as.table = TRUE)
+classifier_metrics
+
+###
+
+performance_mat_expression_NET_NEC = read.table("~/Downloads/Expression_NET_NEC.tsv",header = TRUE,  as.is = TRUE)
+classifier_metrics <- ml_test(performance_mat_expression_NET_NEC[,2], performance_mat_expression_NET_NEC[,1], output.as.table = TRUE)
 classifier_metrics
